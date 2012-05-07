8d0d07b8fa41854a8b719c6690538bbca056d081
root <root@ubuntu.(none)>
2012-05-07 06:10:12 -0700

    fuck

--- QT/ui_mainwindow.h
+++ QT/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Thu May 3 18:36:46 2012
+** Created: Mon May 7 05:56:06 2012
 **      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
